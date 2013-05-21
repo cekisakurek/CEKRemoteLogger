bcde1fcc2bfa5803fb045d8957f5bb532c05efb3
cekisakurek <cekisakurek@gmail.com>
2013-05-21 14:35:28 +0300

    Initial Commit

---
+++ CEKRemoteLogger/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CEKRemoteLogger
+//
+//  Created by cihan emre kisakurek on 5/21/13.
+//  Copyright (c) 2013 cekware. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
